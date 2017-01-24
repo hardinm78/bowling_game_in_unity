b2a3b568e68b6bf41bb983e9242d3b350e0bfc79
hardinm78 <michaelhardin@Michaels-iMac.local>
2017-01-24 10:12:22 -0600

    some tdd

---
+++ Assets/Editor/ActionMasterTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using NUnit.Framework;
+
+public class ActionMasterTest {
+
+	private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+	private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+	private ActionMaster actionMaster;
+	[SetUp]
+	public void Setup(){
+		actionMaster = new ActionMaster();
+	}
+
+	[Test]
+	 public void PassingTest(){
+		Assert.AreEqual (1, 1);
+	 }
+
+	 [Test]
+	 public void T01OneStrikeReturnsEndTurn(){	 	
+		Assert.AreEqual (endTurn,actionMaster.Bowl(10));
+	 }
+	 [Test]
+	 public void T02Bowl8ReturnsTidy(){		
+		Assert.AreEqual (tidy, actionMaster.Bowl (8));
+	 }
+	 [Test]
+	 public void T03Bowl28SpareReturnsEndTurn(){
+		actionMaster.Bowl (8);
+		Assert.AreEqual (endTurn, actionMaster.Bowl (2));
+
+	 }
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster  {
+
+	public enum Action{Tidy,Reset,EndTurn,EndGame}
+//	private int[] bowls = new int[21];
+	private int bowl = 1;
+
+	public Action Bowl(int pins){
+
+	if (pins < 0 || pins >10){
+			throw new UnityException ("invalid pin count");
+	}
+
+	if (pins == 10){
+		return Action.EndTurn;
+			bowl += 2;
+	}
+
+	if (bowl%2!=0){
+			bowl += 1;
+			return Action.Tidy;
+		}else if (bowl%2==0){
+			bowl += 1;
+			return Action.EndTurn;
+	}
+
+		throw new UnityException ("not sure what action to return");
+	}
+
+}
